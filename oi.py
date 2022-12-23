3e7c8261b12e5382aa64f0d8c944ad188356c916
Italo Yuri <74804088+italoyuri@users.noreply.github.com>
2022-12-23 15:45:21 -0300

    Delete oi.py

--- oi.py
+++
@@ -1,9 +0,0 @@
-dia_ini = 24
-dia_fim = 28
-mes = "fevereiro"
-ano = 2017
-
-#"Em 2017 o Carnaval acontece em fevereiro do dia 24 até o dia 28"
-
-print("Em {} o Carnaval acontece em {} do dia {} até o dia {}"
-      .format(ano, mes, dia_ini, dia_fim))
